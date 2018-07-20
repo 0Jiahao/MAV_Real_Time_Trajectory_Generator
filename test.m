7c3cb2c827736fccde2abbfff03c13917f185107
Jiahao Lin <gaaihoulam@outlook.com>
2018-07-20 02:05:20 +0200

    Delete test.m

--- test.m
+++
@@ -1,8 +0,0 @@
-clear; clc; close all;
-% control input u = (T,yaw,pitch,roll)
-% https://en.wikipedia.org/wiki/Aircraft_principal_axes
-obj.position = [0,0,10];
-obj.angle = [pi/2,pi/6,0];
-obj.speed = [0,0,0];        % body frame
-
-plot_3d_obj(obj);
